8f663d9b68c15a77a1462ce16da5fa2f4ee41f82
mrbeean_HP <MrSitiZ@yandex.ru>
2017-03-17 10:27:49 +0300

    change structure, correct path & readme

--- lib/viselitsa.rb
+++ lib/viselitsa.rb
@@ -1,8 +1,8 @@
 current_path = File.dirname(__FILE__)
 
-require_relative "game"
-require_relative "result_printer"
-require_relative "word_reader"
+require_relative "viselitsa/game"
+require_relative "viselitsa/result_printer"
+require_relative "viselitsa/word_reader"
 
 puts "Игра виселица. Версия 3. C чтением данных из файлов. (c) 2014 Mike Butlitsky\n\n"
 
@@ -10,7 +10,7 @@ printer = ResultPrinter.new
 
 word_reader = WordReader.new
 
-words_file_name = "#{current_path}/data/words.txt"
+words_file_name = "#{current_path}/viselitsa/data/words.txt"
 word_reader.read_from_file(words_file_name)
 
 game = Game.new(word_reader.read_from_file(words_file_name))
